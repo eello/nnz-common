87f3dd05be565531632f5fbb8b417edda5ec53f3
jongseong <>
2023-05-07 11:59:54 +0900

    Fix: JwtTokenDecodeResolver 로거 추가

--- src/main/java/io/github/eello/nnz/common/controller/JwtTokenDecodeResolver.java
+++ src/main/java/io/github/eello/nnz/common/controller/JwtTokenDecodeResolver.java
@@ -2,6 +2,8 @@ package io.github.eello.nnz.common.controller;
 
 import io.github.eello.nnz.common.jwt.DecodedToken;
 import io.github.eello.nnz.common.jwt.JwtDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.MethodParameter;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -11,6 +13,8 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 public class JwtTokenDecodeResolver implements HandlerMethodArgumentResolver {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.getParameterType().equals(DecodedToken.class);
@@ -18,12 +22,15 @@ public class JwtTokenDecodeResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        DecodedToken token = null;
+
         String authorization = webRequest.getHeader("Authorization");
         if (StringUtils.hasText(authorization) && authorization.startsWith("Bearer ")) {
             String jwt = authorization.substring(7);
-            return JwtDecoder.decode(jwt);
+            token = JwtDecoder.decode(jwt);
         }
 
-        return null;
+        log.info("REQUEST : jwt = {}", token);
+        return token;
     }
 }
