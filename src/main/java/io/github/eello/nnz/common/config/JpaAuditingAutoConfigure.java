3f6e2806c095facd8d786919744458bbb27b10db
jongseong <>
2023-05-15 00:14:52 +0900

    Feat: JpaAuditor 설정 추가

--- src/main/java/io/github/eello/nnz/common/config/JpaAuditingAutoConfigure.java
+++ src/main/java/io/github/eello/nnz/common/config/JpaAuditingAutoConfigure.java
@@ -1,10 +1,40 @@
 package io.github.eello.nnz.common.config;
 
+import io.github.eello.nnz.common.jwt.DecodedToken;
+import io.github.eello.nnz.common.jwt.JwtDecoder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
-public class JpaAuditingAutoConfigure {
+public class JpaAuditingAutoConfigure implements AuditorAware<Long> {
 
+    private final HttpServletRequest request;
+
+    public JpaAuditingAutoConfigure(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        String authorization = request.getHeader("Authorization");
+        if (StringUtils.hasText(authorization) && authorization.startsWith("Bearer ")) {
+            String jwt = authorization.substring(7);
+            try {
+                DecodedToken token = JwtDecoder.decode(jwt);
+                return Optional.of(token.getId());
+            } catch (UnsupportedEncodingException e) {
+                return Optional.empty();
+            }
+        }
+
+        return Optional.empty();
+    }
 }
